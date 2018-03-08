000d2d7ae597da101e2ea49273545dab6b1c31f8
Splendeurs <obami@et.esiea.fr>
2018-03-08 13:59:39 +0100

    Initial commit

---
+++ src/main/java/fr/esiea/glpoo/kamui/App.java
@@ -0,0 +1,13 @@
+package fr.esiea.glpoo.kamui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
