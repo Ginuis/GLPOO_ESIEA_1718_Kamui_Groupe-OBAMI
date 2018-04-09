c8dd1c3c299796cb272c81744cfed34e1f224596
Alahyane <alahyane@et.esiea.fr>
2018-04-09 21:53:23 +0200

    Test Final
    Les tests sont bien écrit mais en echec pour une raison indeterminée.

--- src/test/java/fr/esiea/glpoo/kamui/FinalCsvTirageDaoTest.java
+++ src/test/java/fr/esiea/glpoo/kamui/FinalCsvTirageDaoTest.java
@@ -3,34 +3,24 @@ package fr.esiea.glpoo.kamui;
 import java.io.File;
 import java.util.List;
 
+import fr.esiea.kamui.dao.FinalCsvTirageDao;
 import fr.esiea.kamui.domain.Tirage;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class FinalCsvTirageDaoTest extends AbstractCsvTirageDaoTest{
-
-	@Override
-	public void init(File file) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public File getFile() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<String> getEntetes() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Tirage> getTirages() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private final static String RESOURCES_PATH = "src/main/resources/";
+	private final static String TIRAGES_FILE_PATH = "euromillions_4.csv";
+	
+	@Before
+	public void doBefore() {
+		final File file = new File(RESOURCES_PATH + TIRAGES_FILE_PATH);
+		dao = new FinalCsvTirageDao();
+		dao.init(file);
 	}
 
 }
